f6fc0d90d8120e9f543b7cda9291fd9a6a7a0a4a
Abdullah Yassine <mickeyamy4@gmail.com>
2023-12-10 15:26:32 -0700

    llast test

--- Chapter_1/constants.cpp
+++ Chapter_1/constants.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
     const double conversion  = 2.54; //since the conversion does not show never, we just keep it constant just in case if someone tried to change it, compiler warns them not to because it is constant
-    const int num_of_students = 20; //testing, another test
+    const int num_of_students = 20; //testing, another test,
 
     
 
